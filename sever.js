51968258256d2b0d7c8c3d16e4320affd76a3313
Pham Minh Toan <minhtoanpham1412@gmail.com>
2024-03-14 14:25:24 +0700

    update

---
+++ sever.js
@@ -0,0 +1,15 @@
+const http = require('http');
+
+const hostname = '127.0.0.1'; // local host
+const port = 8080; // noi chay du an
+
+const server = http.createServer((red, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello World\n Hoi Dan It voi eric');
+});
+
+
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
