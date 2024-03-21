47eff0e7e699dc082190cf74360cd638f5017b67
Pham Minh Toan <minhtoanpham1412@gmail.com>
2024-03-21 11:06:37 +0700

    41

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,51 +1,28 @@
 const { json } = require('express');
 const connection = require('../config/database');
+const { getAllUsers } = require('../services/CRUDServices');
 
-
-const getHomepage = (req, res) => {
-    return res.render('home.ejs')
+const getHomepage = async (req, res) => {
+    let results = await getAllUsers();
+    return res.render('home.ejs', { listusers: results })
 }
 
 const getABC = (req, res) => {
     res.send('Check abc')
-    // let users = [];
-    // connection.query(
-    //     'select * from Users u',
-    //     function (err, result, fields) {
-    //         users = result;
-    //         console.log(">>> Check result: ", result)
-
-    //         console.log(">> check user: ", users);
-    //         res.send(JSON.stringify(users));
-    //     }
-    // );
 }
 
 const getHoitoan = (req, res) => {
-    // res.send('bban Phai giau bang moiu gia')
     res.render('sample.ejs')
 }
 
 const postCreateUser = async (req, res) => {
-    // console.log(">>> req.body: ", req.body)
-
-    let email = req.body.email;
-    let name = req.body.name;
-    let city = req.body.city;
-
-    // let {email, name, city} = req.body
-    // console.log(">>> email: ", email, ", name: ", name, "city: ", city);
-
+    let { email, name, city } = req.body
 
     let [results, , fields] = await connection.query(
-        // ? truyền mảng mảng
         `INSERT INTO Users(email, name, city) 
         VALUES(?, ?, ?)`,
-        [email, name, city],
+        [email, name, city]
     );
-
-    console.log(">>> check results: ", results)
-
     res.send("Create Users success")
 }
 
